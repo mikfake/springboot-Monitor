bed2bc5a99a2b5f7cb9f086b8d5d876b5aa04d7d
342746156@qq.com <342746156@qq.com>
2021-01-04 21:08:00 +0800

    'first'

---
+++ springboot-admin/client/src/main/java/com/mike/monitor/client/controller/ApiController.java
@@ -0,0 +1,16 @@
+package com.mike.monitor.client.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+	
+	@GetMapping("/say")
+	public String hello() {
+		return "hello world";
+	}
+
+}
---
+++ springboot-admin/server/src/main/java/com/mike/monitor/config/SecuritySecureConfig.java
@@ -0,0 +1,32 @@
+package com.mike.monitor.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+
+public class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
+    private final String contextPath;
+
+    public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
+        this.contextPath = adminServerProperties.getContextPath();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 跨域设置，SpringBootAdmin客户端通过instances注册，见InstancesController
+        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .ignoringAntMatchers(contextPath + "/instances");
+
+        http.authorizeRequests().antMatchers(contextPath + "/assets/**").permitAll(); // 静态资源
+        http.authorizeRequests().anyRequest().authenticated(); // 所有请求必须通过认证
+
+        // 整合spring-boot-admin-server-ui
+        http.formLogin().loginPage("/login").permitAll();
+        http.logout().logoutUrl("/logout").logoutSuccessUrl("/login");
+
+        // 启用basic认证，SpringBootAdmin客户端使用的是basic认证
+        http.httpBasic();
+    }
+}
