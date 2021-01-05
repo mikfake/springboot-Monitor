25a95b4acc67de5459380441299f1190888d720f
mike <mike@qq.com>
2021-01-05 17:02:20 +0800

    update

--- springboot-admin/client/src/main/java/com/mike/monitor/client/controller/ApiController.java
+++ springboot-admin/client/src/main/java/com/mike/monitor/client/controller/ApiController.java
@@ -1,16 +1,27 @@
 package com.mike.monitor.client.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 	
+	@Autowired
+	private RestTemplate restTemplate;
 	@GetMapping("/say")
 	public String hello() {
 		return "hello world";
 	}
 
+	
+	@GetMapping("/call")
+	public String call() {
+		ResponseEntity<String> s = restTemplate.getForEntity("http://www.baidu.com", String.class);
+		return s.getBody();
+	}
 }
--- springboot-admin/server/src/main/java/com/mike/monitor/server/config/SecuritySecureConfig.java
+++ springboot-admin/server/src/main/java/com/mike/monitor/server/config/SecuritySecureConfig.java
@@ -1,20 +1,23 @@
-package com.mike.monitor.config;
+package com.mike.monitor.server.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import de.codecentric.boot.admin.server.config.AdminServerProperties;
 
+@Configuration
 public class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
     private final String contextPath;
 
-    public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
+    public SecuritySecureConfig (AdminServerProperties adminServerProperties) {
         this.contextPath = adminServerProperties.getContextPath();
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         // 跨域设置，SpringBootAdmin客户端通过instances注册，见InstancesController
         http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 .ignoringAntMatchers(contextPath + "/instances");
