ea42046e6c087f3efc513e0f6944f0934469b64d
342746156@qq.com <342746156@qq.com>
2021-01-05 20:40:47 +0800

    update

--- springboot-admin/client/src/main/java/com/mike/monitor/client/controller/ApiController.java
+++ springboot-admin/client/src/main/java/com/mike/monitor/client/controller/ApiController.java
@@ -3,6 +3,7 @@ package com.mike.monitor.client.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -23,5 +24,10 @@ public class ApiController {
 	public String call() {
 		ResponseEntity<String> s = restTemplate.getForEntity("http://www.baidu.com", String.class);
 		return s.getBody();
+	}
+	
+	@GetMapping("/test/{num}")
+	public int divisible( @PathVariable(name = "num") int num) {
+		return 5/num;
 	}
 }
